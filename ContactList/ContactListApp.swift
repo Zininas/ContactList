3c00f2a53fd6a19170f1a9dfb954463731fa2d8b
Zininas <zininas@mail.ru>
2024-06-18 16:55:00 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Александр Зинин on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
